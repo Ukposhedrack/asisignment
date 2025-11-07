9d02be4decb23aa35087e01191444ca8d6c577b1
shedrack <ukposhedrack@gmsil.com>
2025-11-07 14:07:52 +0100

    first code
    for you

---
+++ intro.js
@@ -0,0 +1,9 @@
+// Define user information
+const myname = "Avindigh Imoiter Augustine";
+const age = 22;
+const favoriteLanguage = "Python";
+
+// Log the information
+console.log("Name:", myname);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
